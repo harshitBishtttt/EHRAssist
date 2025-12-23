40c56ac36494f69e3285aa63c764fedc614efd3d
Harshit <Harshit@SEZLAP-18771.india.rsystems.com>
2025-12-23 14:19:30 +0530

    condition api null point exc fixed

--- src/main/java/EHRAssist/service/impls/PatientConditionServiceImpl.java
+++ src/main/java/EHRAssist/service/impls/PatientConditionServiceImpl.java
@@ -3,12 +3,14 @@ package EHRAssist.service.impls;
 
 import EHRAssist.dto.response.PatientConditionResponse;
 import EHRAssist.dto.response.patientConditionResponse.*;
+import EHRAssist.model.ConditionMaster;
 import EHRAssist.model.PersonCondition;
 import EHRAssist.repository.PersonConditionRepository;
 import EHRAssist.service.PatientConditionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
@@ -23,9 +25,9 @@ public class PatientConditionServiceImpl implements PatientConditionService {
 
 
     public PatientConditionResponse getPatientCondition(Integer subject,
-                                                       String code,
-                                                       Integer encounter,
-                                                       Pageable pageable) {
+                                                        String code,
+                                                        Integer encounter,
+                                                        Pageable pageable) {
         PatientConditionResponse response = new PatientConditionResponse();
         if ((subject == null) && ((code == null) || code.isEmpty()) && (encounter == null)) {
             return response;
@@ -39,29 +41,33 @@ public class PatientConditionServiceImpl implements PatientConditionService {
         List<Entry> enry = null;
         if (personConditions.isPresent()) {
             List<PersonCondition> conditions = personConditions.get();
+            response.setId(subject.toString());
+            response.setTotal(conditions.size());
             enry = conditions.stream().map(ittr -> {
                 Entry entryObj = new Entry();
-                entryObj.setFullUrl("/baseR4/Condition/" + ittr.getConditionMaster().getIcd9Code());
+                ConditionMaster conditionMaster = ittr.getConditionMaster();
+                entryObj.setFullUrl("/baseR4/Condition/");
                 entryObj.setResource(Resource.builder()
-                                .resourceType("Condition")
+                        .resourceType("Condition")
                         .meta(EntryMeta.builder().lastUpdate(null).source(null).versionId(null).build())
                         .clinicalStatus(ClinicalStatus.builder().build())
                         .verificationStatus(VerificationStatus.builder().build())
+                        .id(ittr.getRowId())
                         .category(Category.builder()
                                 .coding(List.of(Coding.builder()
                                         .system("http://terminology.hl7.org/CodeSystem/condition-category")
-                                        .display(ittr.getConditionMaster().getShortTitle())
-                                        .code(ittr.getConditionMaster().getIcd9Code()).build())).build())
+                                        .display(!ObjectUtils.isEmpty(conditionMaster) ? conditionMaster.getShortTitle() : "")
+                                        .code(!ObjectUtils.isEmpty(conditionMaster) ? conditionMaster.getIcd9Code() : "").build())).build())
                         .encounter(Encounter.builder()
                                 .reference("Encounter/" + ittr.getHadmId()).build())
                         .subject(Subject.builder()
                                 .reference("Patient/" + ittr.getPerson().getSubjectId()).build())
                         .recordedDate(null)
                         .code(Code.builder()
-                                .text(ittr.getConditionMaster().getLongTitle())
+                                .text(!ObjectUtils.isEmpty(ittr.getConditionMaster()) ? ittr.getConditionMaster().getLongTitle() : "")
                                 .coding(List.of(Coding.builder()
-                                        .code(ittr.getConditionMaster().getIcd9Code())
-                                        .display(ittr.getConditionMaster().getLongTitle())
+                                        .code(!ObjectUtils.isEmpty(ittr.getConditionMaster()) ? ittr.getConditionMaster().getIcd9Code() : "")
+                                        .display(!ObjectUtils.isEmpty(ittr.getConditionMaster()) ? ittr.getConditionMaster().getLongTitle() : "")
                                         .system("http://hl7.org/fhir/sid/icd-10-cm")
                                         .build())).build()).build());
                 entryObj.setSearch(Search.builder().mode("matched").build());

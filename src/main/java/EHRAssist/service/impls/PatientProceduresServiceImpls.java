f57a417bc4742e9a357dbdfff4bcf188b7641f9b
Harshit <Harshit@SEZLAP-18771.india.rsystems.com>
2025-12-24 18:10:06 +0530

    re

--- src/main/java/EHRAssist/service/impls/PatientProceduresServiceImpls.java
+++ src/main/java/EHRAssist/service/impls/PatientProceduresServiceImpls.java
@@ -25,7 +25,7 @@ public class PatientProceduresServiceImpls implements PatientProceduresService {
         return Resource.builder()
                 .id((Integer) obj[0])
                 .resourceType("Procedure")
-                .status("complete")
+                .status("completed")
                 .meta(EntryMeta.builder().source("#ep95K1tk1zpP15i7").versionId("1").lastUpdated("2025-12-16T07:13:12.674+00:00").build())
                 .category(Category.builder().coding(List.of(Coding.builder().code((String) obj[9])
                         .display((String) obj[9])
@@ -33,7 +33,7 @@ public class PatientProceduresServiceImpls implements PatientProceduresService {
                 .code(Code.builder().coding(List.of(Coding.builder().system("http://www.cms.gov/codes/cpt")
                         .code("" + obj[5]).display((String) obj[9]).build())).text((String) obj[10]).build())
                 .encounter(Encounter.builder().reference("Encounter/" + obj[2]).build())
-                .subject(Subject.builder().reference("Subject/" + obj[1]).build()).build();
+                .subject(Subject.builder().reference("Patient/" + obj[1]).build()).build();
     }
 
     public PatientProcedureResponse getPatientProcedure(Integer subject, Integer encounter, Integer code, Pageable pageable) {
@@ -47,7 +47,7 @@ public class PatientProceduresServiceImpls implements PatientProceduresService {
             Entry obj = new Entry();
             obj.setFullUrl("https://hapi.fhir.org/baseR4/Procedure/53585775");
             obj.setResource(resourceMapper(ittr));
-            obj.setSearch(Search.builder().mode("matched").build());
+            obj.setSearch(Search.builder().mode("match").build());
             return obj;
         }).toList();
         response.setEntry(entry);

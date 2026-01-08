47ce7f4e4127819f67f389b9bfeebe1c6a559bfb
Harshit <Harshit@SEZLAP-18771.india.rsystems.com>
2026-01-08 12:49:19 +0530

    re

--- src/main/java/EHRAssist/utils/EHRAUtils.java
+++ src/main/java/EHRAssist/utils/EHRAUtils.java
@@ -3,6 +3,7 @@ package EHRAssist.utils;
 import ca.uhn.fhir.context.FhirContext;
 import org.hl7.fhir.r4.model.Bundle;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -18,7 +19,8 @@ public class EHRAUtils {
                 .setPrettyPrint(true)
                 .encodeResourceToString(response);
         return ResponseEntity.ok()
-                .contentType(MediaType.valueOf("application/fhir+json"))
+                .header(HttpHeaders.CONTENT_TYPE, "application/fhir+json; charset=UTF-8")
+                .header(HttpHeaders.CONTENT_ENCODING, "identity")
                 .body(json);
     }
 }
